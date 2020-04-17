90a2bb4ad7340bcfa31e0392e9de75a05f8e1df8
Hao Yang <haoyang.nuaa@gmail.com>
2020-04-17 14:39:32 +0800

    Fix bin-lwe

--- LAC-v3a/clean/bin-lwe.h
+++ LAC-v3a/clean/bin-lwe.h
@@ -1,8 +1,11 @@
+#ifndef BIN_LWE_H
+#define BIN_LWE_H
+
 #include <stdint.h>
 
-//generate the public parameter a from seed
+// generate the public parameter a from seed
 int gen_a(uint8_t *a, const uint8_t *seed);
-//generate the small random vector for secret and error
+// generate the small random vector for secret and error
 int gen_e(uint8_t *e, uint8_t *seed);
 int gen_r(uint8_t *r, uint8_t *seed);
 
@@ -14,3 +17,5 @@ int poly_aff(const uint8_t *a, const  uint8_t *s, uint8_t *e, uint8_t *b, unsign
 int poly_compress(const uint8_t *in, uint8_t *out, const unsigned int vec_num);
 // de-compress: set the low 4bit to be zero
 int poly_decompress(const uint8_t *in, uint8_t *out, const unsigned int vec_num);
+
+#endif
