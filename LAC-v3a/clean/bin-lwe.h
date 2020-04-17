5b0d7c1571fe5dd251b84b1f39b9024c3ae1ba24
Hao Yang <haoyang.nuaa@gmail.com>
2020-04-17 14:40:20 +0800

    Fix trailing whitespace

--- LAC-v3a/clean/bin-lwe.h
+++ LAC-v3a/clean/bin-lwe.h
@@ -11,7 +11,7 @@ int gen_r(uint8_t *r, uint8_t *seed);
 
 // poly_mul  b=[as]
 int poly_mul(const uint8_t *a, const uint8_t *s, uint8_t *b, unsigned int  vec_num);
-// poly_aff  b=as+e 
+// poly_aff  b=as+e
 int poly_aff(const uint8_t *a, const  uint8_t *s, uint8_t *e, uint8_t *b, unsigned int vec_num);
 // compress: cut the low 4bit
 int poly_compress(const uint8_t *in, uint8_t *out, const unsigned int vec_num);
